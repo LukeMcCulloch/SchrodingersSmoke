a85bec973e4847d3b89cc2d12e2d37b829f8aa8a
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-23 22:41:48 -0700

    New Python Implementation started

---
+++ python/ISF.py
@@ -0,0 +1,11 @@
+
+
+import numpy as np
+
+from TorusDEC import TorusDEC
+
+
+class ISF(TorusDEC):
+    
+    def __init__(self):
+        pass
---
+++ python/TorusDEC.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 23 22:39:20 2019
+
+@author: lukemcculloch
+"""
+
+class TorusDEC(object):
+    
+    def __init__(self):
+        pass
+    
