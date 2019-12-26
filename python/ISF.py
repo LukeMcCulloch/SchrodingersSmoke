acb33769cb4720bf6ca8c77299ca1d2cd5efc1c2
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-25 23:55:14 -0700

    interface class for simulating incompressible Schroedinger flow

--- python/ISF.py
+++ python/ISF.py
@@ -6,6 +6,33 @@ from TorusDEC import TorusDEC
 
 
 class ISF(TorusDEC):
+    """
+    ISF a handle class for simulating incompressible Schroedinger flow.
+        SYNTAX
+          isf = ISF(sizex,sizey,sizez,resx,resy,resz)
+      isf = ISF(sizex,sizey,sizez,res)
+        DESCRIPTION
+          ISF is a subclass of TorusDEC handle class. See TorusDEC for calling
+      constructor.
+          To setup, 
+            isf = ISF(5,5,5,32,32,32);  call constructor
+        isf.hbar = 0.1;                 specify Planck constant
+        isf.dt   = 1/24;                specify time step
+        isf.BuildSchroedinger;          this command builds coeff for solving
+                                        Schroedinger equation in Fourier domain
+          Useful functions:
+            [psi1,psi2] = isf.SchroedingerFlow(psi1,psi2)
+            solves Schroedinger equation for (psi1,psi2) for isf.dt time.
+            [psi1,psi2] = isf.Normalize(psi1,psi2)
+            normalizes (psi1,psi2).
+            [psi1,psi2] = isf.PressureProject(psi1,psi2)
+            projects (psi1,psi2) to satisfy divergence free constraint.
+            [vx,vy,vz] = isf.VelocityOneForm(psi1,psi2,isf.hbar)
+            extracts velocity 1-from from (psi1,psi2)
+            [vx,vy,vz] = isf.VelocityOneForm(psi1,psi2)
+            extracts velocity 1-form assuming hbar=1.
+            See also TorusDEC
+    """
     
     def __init__(self):
         pass
