d5bd084e5f6247f7636fbed2575a08a491e23575
Luke McCulloch <tlukemcculloch@gmail.com>
2019-12-27 23:54:53 -0700

    adding particles class for visulalization purposes

---
+++ python/Particles.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 23:53:24 2019
+
+@author: lukemcculloch
+"""
+
+class Particles(object):
+    
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    
